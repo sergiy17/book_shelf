4b5795a7fa31252bc0881d0eba73f5fa803590dd
Sergiy17 <serparkhuts@gmail.com>
2017-06-08 08:41:47 +0300

    Header + Footer

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  # def help
+  #   # @a = 3
+  #   # binding.pry
+  # end
+
+  # def about
+  # end
+
+  # def contact
+  # end
+end
