9d1e4756504f6a3a6b35b7232bba1ade61ea9e80
Sergiy Parkhuts <serparkhuts@gmail.com>
2017-06-08 21:04:51 +0300

    Started with the main part

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title="")
+    @basic_title = "BOOK SHELF"
+    if page_title
+      page_title + " " + @basic_title
+    else
+      @basic_title
+    end
+  end
 end
